d63accd621567d519b617ac1562801d06b74d72b
dilshodwiut <ramatilloyevdilshod7.0@gmail.com>
2021-07-17 12:34:28 +0500

    optimizing js files

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
+const correctAnswers = ["B", "B", "B", "B"];
 const form = document.getElementById("form");
 const notifier = document.querySelector(".notifier");
+const error = document.querySelector(".error");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  validate();
-  animateScrolling(window.scrollX, window.scrollY);
-});
 
-function animateScrolling(X, Y) {
+  let score = 0;
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ];
+
+  if (userAnswers.includes("")) {
+    error.setAttribute("style", "display: block;");
+    notifier.setAttribute("style", "display: none;");
+  } else {
+    error.setAttribute("style", "display: none;");
+    notifier.setAttribute("style", "display: block;");
+
+    userAnswers.forEach((answer, index) => {
+      answer == correctAnswers[index] ? (score += 25) : score;
+    });
+
+    let output = 0;
+    const timer = setInterval(() => {
+      notifier.querySelector("span").textContent = `${output}%`;
+      if (output == score) {
+        clearInterval(timer);
+      } else {
+        output++;
+      }
+    }, 10);
+  }
+
   const interval = setInterval(() => {
-    if (window.scrollX == 0 && window.scrollY == 0) {
+    if (scrollX == 0 && scrollY == 0) {
       clearInterval(interval);
     } else {
-      window.scrollTo(window.scrollX - 10, window.scrollY - 10);
+      scrollBy(-50, -50);
     }
-  }, 1);
-}
+  }, 10);
+});
